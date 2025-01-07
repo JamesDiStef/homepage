9d49a1f03a3ff50a6a3600c1770418c56193ed03
James <james.distef@gmail.com>
2025-01-07 15:22:38 -0500

    add cards, begin to build out page

---
+++ src/app/home/link-card/link-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-link-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './link-card.component.html',
+  styleUrl: './link-card.component.css',
+})
+export class LinkCardComponent {
+  @Input()
+  title!: string;
+  @Input() description!: string;
+  @Input() link!: string;
+}
